77d2fba794d693cd808edf472d45e51a7de0bccf
kema-ray <rachelkemuma99@gmail.com>
2022-11-10 10:22:51 +0300

    Create a custom user

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from helpers.models import TrackingModel
+from django.contrib.auth.models import PermissionsMixin,AbstractBaseUser,BaseUserManager
 
-# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin, TrackingModel):
+    pass
